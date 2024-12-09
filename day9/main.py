d4c788cc799516a52de27591751819a03e1ac589
Nikola Đipanov <nikola.djipanov@gmail.com>
2024-12-09 21:54:41 +0000

    works fast - but not correct

--- day9/main.py
+++ day9/main.py
@@ -2,22 +2,23 @@
 from __future__ import annotations
 
 import itertools
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import heapq
+from sortedcontainers import SortedList
 
 
 @dataclass(order=True)
 class Span:
-    f_id: int | None
     len: int
     pos: int
-    next: Span | None = None
-    prev: Span | None = None
+    f_id: int | None
+    next: Span | None = field(compare=False, default=None)
+    prev: Span | None = field(compare=False, default=None)
 
 
 @dataclass
 class Disk:
-    empty_spans: list[tuple[(int, int), Span]]
+    empty_spans: SortedList[Span]
     f_by_id: dict[int, Span]
     start: Span | None
     end: Span | Disk | None = None
@@ -33,21 +34,18 @@ class Disk:
             s.prev = self.end
             self.end = s
         if s.f_id is None:
-            heapq.heappush(self.empty_spans, s)
+            self.empty_spans.add(s)
         else:
             self.f_by_id[s.f_id] = s
 
     def find_empty_for(self, s: Span) -> Span | None:
         e = None
-        unused = []
-        while self.empty_spans:
-            maybe_e = heapq.heappop(self.empty_spans)
+        idx = self.empty_spans.bisect_left(Span(s.len, -1, None))
+        for maybe_e in self.empty_spans[idx:]:
             if maybe_e.len >= s.len and maybe_e.pos < s.pos:
                 e = maybe_e
+                self.empty_spans.remove(e)
                 break
-            unused.append(maybe_e)
-        for u in unused:
-            heapq.heappush(self.empty_spans, u)
         return e
 
     def fit_into(self, empty: Span, s: Span):
@@ -70,29 +68,14 @@ class Disk:
         empty.pos, s.pos = s.pos, empty.pos
 
         if empty.len > s.len:
-            new_empty = Span(None, empty.len - s.len, s.pos + s.len)
+            new_empty = Span(empty.len - s.len, s.pos + s.len, None)
             new_empty.prev = s
             new_empty.next = s.next
             s.next = new_empty
             new_empty.next.prev = new_empty
-            heapq.heappush(self.empty_spans, new_empty)
+            self.empty_spans.add(new_empty)
         # Reduce the size of the old empty that is now moved
         empty.len = s.len
-
-    def merge_empty_from(self, s: Span) -> None:
-        assert s.f_id is None
-        cur = s
-        while cur.next and cur.next.f_id is None:
-            cur.len += cur.next.len
-            cur.next = cur.next.next
-            if cur.next:
-                cur.next.prev = cur
-
-    def fixup_pos_from(self, e: Span):
-        cur = e
-        while cur.next:
-            cur.next.pos = cur.pos + cur.len
-            cur = cur.next
 
     def validate_pos(self):
         cur = self.start
@@ -104,7 +87,6 @@ class Disk:
         e = self.find_empty_for(s)
         if e:
             self.fit_into(e, s)
-            self.merge_empty_from(e.prev if e.prev and e.prev.f_id is None else e)
             self.validate_pos()
 
     def defrag(self):
@@ -149,14 +131,14 @@ def parse_diskmap(diskmap):
 def parse_diskmap2(diskmap):
     empty = False
     f_id = 0
-    disk = Disk([], {}, None)
+    disk = Disk(SortedList(), {}, None)
     pos = 0
     for s in diskmap:
         l = int(s)
         if empty:
-            s = Span(None, l, pos)
+            s = Span(l, pos, None)
         else:
-            s = Span(f_id, l, pos)
+            s = Span(l, pos, f_id)
             f_id += 1
         disk.append(s)
         empty = not empty
@@ -199,4 +181,5 @@ if __name__ == "__main__":
     disk = parse_diskmap2(diskmap)
     print(disk.print_layout())
     disk.defrag()
+    print(disk.print_layout())
     print(disk.checksum())
